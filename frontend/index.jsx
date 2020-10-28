33ba5ead4663728ebcc07535311ad215e6d87658
Brendon <brendon.biagi@gmail.com>
2020-10-28 10:38:34 -0400

    change skeleton for backend

---
+++ frontend/index.jsx
@@ -0,0 +1,12 @@
+import Game from './game.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './root'
+
+document.addEventListener("DOMContentLoaded", function() {
+    const root = document.getElementById("root");
+    const game = new Game(root)
+    game.runGame()
+
+    ReactDOM.render(game, root)
+})
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Game from './game';
-
-document.addEventListener("DOMContentLoaded", function() {
-    const root = document.getElementById("root");
-    const game = new Game(root)
-    game.runGame()
-
-})
