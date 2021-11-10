36b157c03c5025ad494b82dbea325684fbb3978e
Brendon <brendon.biagi@gmail.com>
2021-11-10 13:44:39 -0500

    create skeleton part 1

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';  
+import { Route } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../utils/route_util';
+
+export default () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
