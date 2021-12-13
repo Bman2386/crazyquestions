c4516a14c77dcaceeb16635a43b63f55b26db686
Brendon <brendon.biagi@gmail.com>
2021-12-13 15:24:28 -0500

    start navbar skeleton

---
+++ frontend/components/navbar/navbarContainer.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import NavBar from './navbar';
+
+
+const mapStateToProps = (state) => {
+    return {
+        
+    }
+};
+
+const mapDispatchToProps = (dispatch) => ({
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
