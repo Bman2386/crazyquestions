7ebfaa730d05765be389e43eaf1c682e746aa8ac
Brendon <brendon.biagi@gmail.com>
2021-12-15 17:25:19 -0500

    add profile skeleton

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 import navBarContainer from './navbar/navbarContainer';
 import homeContainer from './home/homeContainer';
+import profileContainer from './profile/profileContainer';
 //make  game, and game-boards
 export default () => {
     return (
         <div>
             <Route path="/" component={navBarContainer} />
             <Route exact path="/" component={homeContainer} />
+            <ProtectedRoute path='/profile' component={profileContainer} />
         </div>
     )
 };
