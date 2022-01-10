21a2b3834febede06b781e085f522dfab807f936
Brendon <brendon.biagi@gmail.com>
2022-01-10 16:42:55 -0500

    add components to login page

--- frontend/components/session/loginContainer.js
+++ frontend/components/session/loginContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import Login from './login';
-
+import { createNewUser, login } from '../../actions/session';
 
 const mapStateToProps = (state) => {
     return {
@@ -9,6 +9,8 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
+    create: createNewUser,
+    login: login
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
